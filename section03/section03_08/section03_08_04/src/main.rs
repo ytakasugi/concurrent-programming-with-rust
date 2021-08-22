e5fd14ff06df2ca09fcf1fe3e2bbd3369211d361
ytakasugi <sh7.tibi0129@gmail.com>
2021-08-22 15:00:53 +0900

    new cargo project

---
+++ section03/section03_08/section03_08_04/src/main.rs
@@ -0,0 +1,23 @@
+use std::sync::{Arc, Barrier};
+use std::thread;
+
+fn main() {
+    // スレッドハンドラを保存するベクタ
+    let mut v = Vec::new();
+
+    // 10スレッド分のバリア同期をArcで包む
+    let barrier = Arc::new(Barrier::new(10));
+
+    // 10スレッド起動
+    for _ in 0..10 {
+        let b = barrier.clone();
+        let th = thread::spawn(move || {
+            b.wait();
+            println!("finished barrier");
+        });
+        v.push(th);
+    }
+    for th in v {
+        th.join().unwrap();
+    }
+}
