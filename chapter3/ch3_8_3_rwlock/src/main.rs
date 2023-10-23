dce7e2036b220effe904cacbf5a20abf6687264a
ytakasugi <sh7.tibi0129@gmail.com>
2023-10-23 23:17:15 +0900

    change file

--- chapter3/ch3_8_3_rwlock/src/main.rs
+++ chapter3/ch3_8_3_rwlock/src/main.rs
@@ -1,3 +1,21 @@
+use std::sync::RwLock;
+
 fn main() {
-    println!("Hello, world!");
+    let lock = RwLock::new(10);
+    {
+        // immutableな参照を取得
+        // Read Lockは何度でも行える。
+        let v1 = lock.read().unwrap();
+        let v2 = lock.read().unwrap();
+        println!("v1 = {}", v1);
+        println!("v2 = {}", v2);
+    }
+
+    {
+        // mutableな参照を取得
+        // Write Lockはスコープ内で1つのみ
+        let mut v = lock.write().unwrap();
+        *v = 7;
+        println!("v = {}", v);
+    }
 }
--- chapter3/ch3_8_4_barrier/src/main.rs
+++ chapter3/ch3_8_4_barrier/src/main.rs
@@ -1,3 +1,22 @@
+use std::sync::{Arc, Barrier};
+use std::thread;
+
 fn main() {
-    println!("Hello, world!");
+    // スレッドハンドラを保存するベクタ
+    let mut v = Vec::new();
+    // 10スレッド分のバリア同期をArcで包む
+    let barrier = Arc::new(Barrier::new(10));
+
+    for _ in 0..10 {
+        let b = barrier.clone();
+        let th = thread::spawn(move || {
+            // すべてのスレッドが合流するまでスレッドをブロックする
+            b.wait();
+            println!("finished barrier");
+        });
+        v.push(th);
+    }
+    for th in v {
+        th.join().unwrap();
+    }
 }
