0b83c16a42397f6dcfeed1aaabe4673c323b98e2
ytakasugi <sh7.tibi0129@gmail.com>
2023-10-26 23:32:01 +0900

    change files

--- chapter3/ch3_8_5_semaphore/src/main.rs
+++ chapter3/ch3_8_5_semaphore/src/main.rs
@@ -4,9 +4,9 @@ use semaphore::Semaphore;
 use std::sync::atomic::{AtomicUsize, Ordering};
 use std::sync::Arc;
 
-const NUM_LOOP: usize = 10;
-const NUM_THREADS: usize = 10;
-const SEM_NUM: isize = 2;
+const NUM_LOOP: usize = 5;
+const NUM_THREADS: usize = 2;
+const SEM_NUM: isize = 1;
 
 static mut CNT: AtomicUsize = AtomicUsize::new(0);
 
@@ -22,11 +22,10 @@ fn main() {
                 // セマフォ内のカウンタをインクリメント
                 // もし、カウンタがSEM_NUM以上であれば、カウンタがSEM_NUM未満になるまで待機
                 s.wait();
-                let thread_id = std::thread::current().id();
                 // `fetch_add`で古い値を読み、それに加算した値を書き込み、古い値を返す
                 unsafe { CNT.fetch_add(1, Ordering::SeqCst) };
                 let n = unsafe { CNT.load(Ordering::SeqCst) };
-                println!("{:?}, semaphore: i = {}, CNT = {}", thread_id, i, n);
+                println!("{:?}, semaphore: i = {}, CNT = {}", std::thread::current().id(), i, n);
                 assert!((n as isize) <= SEM_NUM);
                 // `fetch_sub`で古い値を読み、それを減算した値を書き込み、古い値を返す
                 unsafe { CNT.fetch_sub(1, Ordering::SeqCst) };
