5ee12c829d0e722ccb66e4113c44133f5e1d5e31
ytakasugi <sh7.tibi0129@gmail.com>
2023-11-05 00:05:08 +0900

    change files

---
+++ chapter4/chapter4-4/ch4_4_reent_rust/src/main.rs
@@ -0,0 +1,12 @@
+use std::sync::{Arc, Mutex};
+
+fn main() {
+    let lock0 = Arc::new(Mutex::new(0));
+    let lock1 = lock0.clone();
+
+    let a = lock0.lock().unwrap();
+    let b = lock1.lock().unwrap();
+
+    println!("{}", a);
+    println!("{}", b);
+}
