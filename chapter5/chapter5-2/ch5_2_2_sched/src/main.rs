14541db048c223b4f079ec96520ec85cf1430568
ytakasugi <sh7.tibi0129@gmail.com>
2023-11-12 23:03:34 +0900

    change files

--- chapter5/chapter5-2/ch5_2_2_sched/src/main.rs
+++ chapter5/chapter5-2/ch5_2_2_sched/src/main.rs
@@ -1,3 +1,125 @@
+use futures::future::{BoxFuture, FutureExt};
+use futures::task::{waker_ref, ArcWake};
+use std::future::Future;
+use std::pin::Pin;
+use std::sync::mpsc::{sync_channel, Receiver, SyncSender};
+use std::sync::{Arc, Mutex};
+use std::task::{Context, Poll};
+
+struct Hello {
+    state: StateHello,
+}
+
+// 状態
+enum StateHello {
+    HELLO,
+    WORLD,
+    END,
+}
+
+impl Hello {
+    fn new() -> Self {
+        Hello {
+            state: StateHello::HELLO,
+        }
+    }
+}
+
+impl Future for Hello {
+    type Output = ();
+
+    fn poll(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<()> {
+        match (*self).state {
+            StateHello::HELLO => {
+                print!("Hello, ");
+                (*self).state = StateHello::WORLD;
+                cx.waker().wake_by_ref(); // 自身を実行キューにエンキュー
+                return Poll::Pending;
+            }
+            StateHello::WORLD => {
+                println!("World!");
+                (*self).state = StateHello::END;
+                cx.waker().wake_by_ref(); // 自身を実行キューにエンキュー
+                return Poll::Pending;
+            }
+            StateHello::END => {
+                return Poll::Ready(());
+            }
+        }
+    }
+}
+
+struct Task {
+    // 実行するコルーチン
+    future: Mutex<BoxFuture<'static, ()>>,
+    // Executorへスケジューリングするためのチャネル
+    sender: SyncSender<Arc<Task>>,
+}
+
+impl ArcWake for Task {
+    fn wake_by_ref(arc_self: &Arc<Self>) {
+        // 自身をスケジューリング
+        let self0 = arc_self.clone();
+        arc_self.sender.send(self0).unwrap();
+    }
+}
+
+struct Executor {
+    // 実行キュー
+    sender: SyncSender<Arc<Task>>,
+    receiver: Receiver<Arc<Task>>,
+}
+
+impl Executor {
+    fn new() -> Self {
+        // チャネルを生成。キューのサイズは最大1024個
+        let (sender, receiver) = sync_channel(1024);
+        Executor {
+            sender: sender.clone(),
+            receiver,
+        }
+    }
+
+    // 新たにTaskを生成するためのSpawnerを作成
+    fn get_spawner(&self) -> Spawner {
+        Spawner {
+            sender: self.sender.clone(),
+        }
+    }
+
+    fn run(&self) {
+        // チャネルからTaskを受信して順に実行
+        while let Ok(task) = self.receiver.recv() {
+            // コンテキストを生成
+            let mut future = task.future.lock().unwrap();
+            let waker = waker_ref(&task);
+            let mut ctx = Context::from_waker(&waker);
+            // pollを呼び出し実行
+            let _ = future.as_mut().poll(&mut ctx);
+        }
+    }
+}
+
+struct Spawner {
+    sender: SyncSender<Arc<Task>>,
+}
+
+impl Spawner {
+    fn spawn(&self, future: impl Future<Output = ()> + 'static + Send) {
+        let future = future.boxed();    // FutureをBox化
+        let task = Arc::new(Task {      // Task生成
+            future: Mutex::new(future),
+            sender: self.sender.clone(),
+        });
+
+        // 実行キューにエンキュー
+        self.sender.send(task).unwrap();
+    }
+}
+
+
 fn main() {
-    println!("Hello, world!");
+    let executor = Executor::new();
+    executor.get_spawner().spawn(Hello::new());
+    executor.run();
 }
--- chapter5/chapter5-3/ch5_3_2_ioselect/src/main.rs
+++ chapter5/chapter5-3/ch5_3_2_ioselect/src/main.rs
@@ -1,3 +1,32 @@
+use futures::{
+    future::{BoxFuture, FutureExt},
+    task::{waker_ref, ArcWake},
+};
+use nix::{
+    errno::Errno,
+    sys::{
+        epoll::{
+            epoll_create1, epoll_ctl, epoll_wait,
+            EpollCreateFlags, EpollEvent, EpollFlags, EpollOp,
+        },
+        eventfd::{eventfd, EfdFlags},
+    },
+    unistd::{read, write},
+};
+use std::{
+    collections::{HashMap, VecDeque},
+    future::Future,
+    io::{BufRead, BufReader, BufWriter, Write},
+    net::{SocketAddr, TcpListener, TcpStream},
+    os::unix::io::{AsRawFd, RawFd},
+    pin::Pin,
+    sync::{
+        mpsc::{sync_channel, Receiver, SyncSender},
+        Arc, Mutex,
+    },
+    task::{Context, Poll, Waker},
+};
+
 fn main() {
     println!("Hello, world!");
 }
