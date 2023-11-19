6801d1dff76b3610b9f82a295521557f943da757
ytakasugi <sh7.tibi0129@gmail.com>
2023-11-19 23:25:47 +0900

    change file

--- chapter5/chapter5-3/ch5_3_2_ioselect/src/main.rs
+++ chapter5/chapter5-3/ch5_3_2_ioselect/src/main.rs
@@ -11,7 +11,7 @@ use nix::{
         },
         eventfd::{eventfd, EfdFlags},
     },
-    unistd::{read, write},
+    unistd::{read, write}, libc::epoll_create1,
 };
 use std::{
     collections::{HashMap, VecDeque},
@@ -26,6 +26,56 @@ use std::{
     },
     task::{Context, Poll, Waker},
 };
+
+fn write_eventfd(fd: RawFd, n: usize) {
+    // usizeを*const u8に変換
+    let ptr = &n as *const usize as *const u8;
+    // ポインタと長さからスライスを作成する
+    let val = unsafe {
+        std::slice::from_raw_parts(
+            prt, std::mem::size_of_val(&n)
+        )
+    };
+    // writeシステムコール呼び出し
+    write(fd, &val).unwrap();
+}
+
+enum IOOps {
+    // epollへ追加
+    ADD(EpollCreateFlags, RawFd, Waker),
+    // epollから削除
+    REMOVE(RawFd),
+}
+
+struct IOSelector {
+    // fdからwaker
+    wakers: Mutex<HashMap<RawFd, Waker>>,
+    // IOのキュー
+    queue: Mutex<VecDeque<IOOps>>,
+    // epollのfd
+    epfd: RawFd,
+    // eventfdのfd
+    event: RawFd,
+}
+
+impl IOSelector {
+    fn new() -> Arc<Self> {
+        let s = IOSelector {
+            wakers: Mutex::new(HashMap::new()),
+            queue: Mutex::new(VecDeque::new()),
+            epfd: epoll_create1(EpollCreateFlags::empty()).unwrap(),
+            // eventfd生成
+            event: eventfd(0, EfdFlags::empty()).unwrap(),
+        };
+        let result = Arc::new(s);
+        let s = result.clone();
+        // epoll用スレッドを生成
+        std::thread::spawn(move || s.select());
+
+        result
+    }
+}
+
 
 fn main() {
     println!("Hello, world!");
