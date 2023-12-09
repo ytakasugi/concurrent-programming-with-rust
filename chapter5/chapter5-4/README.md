3fa5a6524a7e77b596a6a0b3b14601622adb1329
ytakasugi <sh7.tibi0129@gmail.com>
2023-12-10 00:00:56 +0900

    Change file

---
+++ chapter5/chapter5-4/README.md
@@ -0,0 +1,5 @@
+# Chapter5-4
+
+```text
+socat stdio tcp:localhost:10000
+```
--- chapter5/chapter5-4/ch5_4_tokio/src/main.rs
+++ chapter5/chapter5-4/ch5_4_tokio/src/main.rs
@@ -1,3 +1,41 @@
-fn main() {
-    println!("Hello, world!");
+use tokio::io::{AsyncBufReadExt, AsyncWriteExt};
+use tokio::io;
+use tokio::net::TcpListener; 
+
+#[tokio::main]
+async fn main() -> io::Result<()> {
+    let listener = TcpListener::bind("127.0.0.1:10000").await.unwrap();
+
+    loop {
+        // コネクションをアクセプト
+        let (mut socket, addr) = listener.accept().await?;
+        println!("accept: {}", addr);
+
+        // 非同期タスクを生成
+        tokio::spawn(async move {
+            let (r, w) = socket.split();
+            let mut reader = io::BufReader::new(r);
+            let mut writer = io::BufWriter::new(w);
+
+            let mut line = String::new();
+            loop {
+                line.clear();
+                match reader.read_line(&mut line).await {
+                    Ok(0) => { // コネクションクローズ
+                        println!("closed: {}", addr);
+                        return;
+                    }
+                    Ok(_) => {
+                        print!("read: {}, {}", addr, line);
+                        writer.write_all(line.as_bytes()).await.unwrap();
+                        writer.flush().await.unwrap();
+                    }
+                    Err(e) => { // エラー
+                        println!("error: {}, {}", addr, e);
+                        return;
+                    }
+                }
+            }
+        });
+    }
 }
