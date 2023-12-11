42e27daa391fbca1417b748ff8d02f8276e467d9
ytakasugi <sh7.tibi0129@gmail.com>
2023-12-11 22:38:30 +0900

    change file

--- chapter5/chapter5-4/ch5_4_sleep/src/main.rs
+++ chapter5/chapter5-4/ch5_4_sleep/src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+// use std::{thread, time};
+
+// #[tokio::main]
+// async fn main() {
+//     tokio::join!(async move {
+//         let tec_secs = time::Duration::from_secs(10);
+//         thread::sleep(tec_secs);
+//     });
+// }
+
+
+use std::time;
+
+#[tokio::main]
+async fn main() {
+    tokio::join!(async move {
+        let ten_secs = time::Duration::from_secs(10);
+        tokio::time::sleep(ten_secs).await;
+    });
 }
