5f6745da08608c0b71702018e97be2cb0155fc07
ytakasugi <sh7.tibi0129@gmail.com>
2023-12-17 00:06:25 +0900

    Change files

--- chapter5/chapter5-4/ch5_4_mutex_1/src/main.rs
+++ chapter5/chapter5-4/ch5_4_mutex_1/src/main.rs
@@ -1,3 +1,33 @@
-fn main() {
-    println!("Hello, world!");
+use std::sync::{Arc, Mutex};
+
+const NUM_TASKS: usize = 4;
+const NUM_LOOPS: usize = 1000;
+
+#[tokio::main]
+async fn main() -> Result<(), tokio::task::JoinError> {
+    let val = Arc::new(Mutex::new(0));
+    let mut v = Vec::new();
+
+    for _ in 0..NUM_TASKS {
+        let n = val.clone();
+        let t = tokio::spawn(async move {
+            for _ in 0..NUM_LOOPS {
+                let mut n0 = n.lock().unwrap();
+                *n0 += 1;
+            }
+        });
+        v.push(t);
+    }
+
+    for i in v {
+        i.await?;
+    }
+
+    println!(
+        "COUNT = {} (expected = {})",
+        *val.lock().unwrap(),
+        NUM_LOOPS * NUM_TASKS
+    );
+
+    Ok(())
 }
--- chapter5/chapter5-4/ch5_4_mutex_2/src/main.rs
+++ chapter5/chapter5-4/ch5_4_mutex_2/src/main.rs
@@ -1,3 +1,39 @@
-fn main() {
-    println!("Hello, world!");
+use std::{sync::Arc, time};
+use tokio::sync::Mutex;
+
+const NUM_TASKS: usize = 8;
+
+// ロックだけするタスク
+async fn lock_only(v: Arc<Mutex<u64>>) {
+    let mut n = v.lock().await;
+    *n += 1;
+}
+
+// ロック中にawaitを行うタスク
+async fn lock_sleep(v: Arc<Mutex<u64>>) {
+    let mut n = v.lock().await;
+    let ten_secs = time::Duration::from_secs(10);
+    tokio::time::sleep(ten_secs).await; 
+    *n += 1;
+}
+
+#[tokio::main]
+async fn main() -> Result<(), tokio::task::JoinError> {
+    let val = Arc::new(Mutex::new(0));
+    let mut v = Vec::new();
+
+    let t = tokio::spawn(lock_sleep(val.clone()));
+    v.push(t);
+
+    for _ in 0..NUM_TASKS {
+        let n = val.clone();
+        let t = tokio::spawn(lock_only(n));
+        v.push(t);
+    }
+
+    for i in v {
+        i.await?;
+    }
+
+    Ok(())
 }
